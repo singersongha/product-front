e39d8809734795dc2dde7c74d6b9ecb15ea8d2c0
singersongha <songha714@naver.com>
2024-11-01 13:50:17 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ const initProduct = {
   price: 0,
 };
 
-axios.defaults.baseURL = import.meta.env.PROD || "http://localhost:8080";
+axios.defaults.baseURL = import.meta.env.PROD
+  ? location.hostname
+  : "http://localhost:8080";
 
 const getAllProductsApi = async () => {
   const { data } = await axios({
